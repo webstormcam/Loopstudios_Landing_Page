53bc51ff44e6bbbe63c66b2ee14182d027b93603
Cameron Chandler <webstormcam@gmail.com>
2022-02-16 14:02:53 -0500

    Made the menu transition in and out

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ let drop = document.getElementById('dropdown')
 ham.addEventListener('click',function(){
 if(ham.getAttribute('src') ==='imgs/icon-close.svg'){
     ham.src = 'imgs/icon-hamburger.svg'
-    drop.style.display='none'
+    drop.classList.remove('animate__fadeIn')
+    drop.classList.add('animate__fadeOut')
+    // drop.style.display='none'
+    
     
  
 } else{
     ham.src = 'imgs/icon-close.svg'
     drop.style.display='flex'
+    drop.classList.add('animate__fadeIn')
+    drop.classList.remove('animate__fadeOut')
+
     
 }
 });
