28b8328edbf441e99ae2497c9b4fe0ab81919bb2
Cameron Chandler <webstormcam@gmail.com>
2023-04-20 22:15:03 -0400

    fixed the hidden text bug :)

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ if(ham.getAttribute('src') ==='imgs/icon-close.svg'){
     ham.src = 'imgs/icon-hamburger.svg'
     drop.classList.remove('animate__fadeIn')
     drop.classList.add('animate__fadeOut')
-    // drop.style.display='none'
+   
+    setTimeout(function(){
+  drop.style.display='none'
+    },500)
     
     
  
